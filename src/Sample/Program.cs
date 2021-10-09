53a7470a7d39c1deae04c44bba36d6067759cb36
Simon Cropp <simon.cropp@gmail.com>
2021-10-09 20:45:47 +1100

    cleanup

--- src/Sample/Program.cs
+++ src/Sample/Program.cs
@@ -1,31 +1,23 @@
-﻿using System;
-using System.Threading.Tasks;
-using NServiceBus;
+﻿using NServiceBus;
 using NServiceBus.MsgPack;
 
-class Program
+var configuration = new EndpointConfiguration("MsgPackSerializerSample");
+configuration.UseSerialization<MsgPackSerializer>();
+configuration.UsePersistence<InMemoryPersistence>();
+configuration.UseTransport<LearningTransport>();
+
+var endpoint = await Endpoint.Start(configuration);
+
+var message = new MyMessage
 {
-    static async Task Main()
-    {
-        var configuration = new EndpointConfiguration("MsgPackSerializerSample");
-        configuration.UseSerialization<MsgPackSerializer>();
-        configuration.UsePersistence<InMemoryPersistence>();
-        configuration.UseTransport<LearningTransport>();
-
-        var endpoint = await Endpoint.Start(configuration);
-
-        var message = new MyMessage
-        {
-            DateSend = DateTime.Now,
-        };
-        await endpoint.SendLocal(message);
-        var startSaga = new StartSaga
-        {
-            TheId = Guid.NewGuid(),
-        };
-        await endpoint.SendLocal(startSaga);
-        Console.WriteLine("Press any key to stop program");
-        Console.Read();
-        await endpoint.Stop();
-    }
-}
+    DateSend = DateTime.Now,
+};
+await endpoint.SendLocal(message);
+var startSaga = new StartSaga
+{
+    TheId = Guid.NewGuid(),
+};
+await endpoint.SendLocal(startSaga);
+Console.WriteLine("Press any key to stop program");
+Console.Read();
+await endpoint.Stop();
